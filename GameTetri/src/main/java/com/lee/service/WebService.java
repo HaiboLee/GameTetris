d82a4c5060bb46535d76075471a4a7638e4f08ea
Lee <lihaiboloveyou@live.com>
2016-12-16 08:57:25 +0800

    基础框架

---
+++ GameTetri/src/main/java/com/lee/service/WebService.java
@@ -0,0 +1,11 @@
+package com.lee.service;
+
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * Created by Lee on 2016/12/16.
+ */
+@ServerEndpoint("/WebSocket")
+
+public class WebService {
+}
